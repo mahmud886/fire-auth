fe725b7163b45fb11bf9946fc83265756b7bb110
mahmud886 <iqbal886mahmud@gmail.com>
2021-09-20 20:57:17 +0600

    inital firebase auth added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { initializeApp } from 'firebase/app';
+import {
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider,
+    signOut,
+} from 'firebase/auth';
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+
+const app = initializeApp(firebaseConfig);
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user, setUser] = useState({
+        isSignedIn: false,
+        name: '',
+        email: '',
+        photo: '',
+    });
+
+    const provider = new GoogleAuthProvider();
+
+    const handleSign = () => {
+        const auth = getAuth();
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                console.log(result);
+                const { displayName, email, photoURL } = result.user;
+                const signedInUser = {
+                    isSignedIn: true,
+                    name: displayName,
+                    email: email,
+                    photo: photoURL,
+                };
+                setUser(signedInUser);
+                console.log(displayName, email, photoURL);
+            })
+            .catch((err) => {
+                console.log(err);
+                console.log(err.message);
+            });
+    };
+
+    const handleSignOut = () => {
+        const auth = getAuth();
+        signOut(auth)
+            .then((result) => {
+                const signedOutUser = {
+                    isSignedIn: false,
+                    name: '',
+                    email: '',
+                    photo: '',
+                };
+                setUser(signedOutUser);
+            })
+            .catch((err) => {
+                console.log(err);
+                console.log(err.message);
+            });
+    };
+
+    return (
+        <div className='App'>
+            {user.isSignedIn ? (
+                <button onClick={handleSignOut}>Sign Out</button>
+            ) : (
+                <button onClick={handleSign}>Sign In</button>
+            )}
+            {user.isSignedIn && (
+                <div>
+                    <p>Welcome. {user.name}</p>
+                    <p>Your email: {user.email}</p>
+                    <img src={user.photo} alt='' />
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyA1vDTQUefnL9rYkDtaA2QPnYQAz63Ytgs',
+    authDomain: 'user-crud-fdb40.firebaseapp.com',
+    projectId: 'user-crud-fdb40',
+    storageBucket: 'user-crud-fdb40.appspot.com',
+    messagingSenderId: '183355541735',
+    appId: '1:183355541735:web:5335fdcaa5be1409317a42',
+};
+
+export default firebaseConfig;
